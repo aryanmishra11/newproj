f39ccb0c02ee3a20305a160acc1fa34f29d530b8
aryanmishra11 <aryanmishra441@gmail.com>
2024-05-15 11:41:09 +0530

    improved Context

--- Nlp/app.py
+++ Nlp/app.py
@@ -6,8 +6,11 @@ from langchain_core.messages import AIMessage,HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
+import google.generativeai as genai
+import mysql.connector
 import getpass
 import os
+
 
 if "GOOGLE_API_KEY" not in os.environ:
     os.environ["GOOGLE_API_KEY"] = getpass.getpass("GOOGLE_API_KEY")
@@ -25,10 +28,10 @@ def init_database(user:str,password:str,host:str,port:str,database:str)->SQLData
     db_uri=f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
     return SQLDatabase.from_uri(db_uri)
 
-    
+ 
 def get_sql_chain(db):
     template="""
-    You are a Data Analyst at a company. You are interactig with a user who is asking you questions about the company's database. Based on the table schema below, write a SQL query tat would answer the user's question. Take the conversation history into accout.
+    You are a Data Analyst at a company. You are interacting with a user who is asking you questions about the company's database. Based on the table schema below, write a SQL query that would answer the user's question. Take the conversation history into accout.
     <SCHEMA>{schema}</SCHEMA>
     
     Write only the SQL query and nothing else. Do not wrap the SQL Query in any other text, not even backticks,
@@ -44,7 +47,7 @@ def get_sql_chain(db):
     Question: {question}
     SQL Query:
     """
-    
+
     prompt=ChatPromptTemplate.from_template(template)
     llm = ChatGoogleGenerativeAI(model="gemini-pro")
     
@@ -57,21 +60,52 @@ def get_sql_chain(db):
         |llm
         |StrOutputParser()
     )
+
+
+
+def is_valid(user_query,db):
     
+    def get_schemas(_):
+        return db.get_table_info()
+    
+    prompt=f"""you are a SQL expert you can tell if the question asked by the user in natural language is related to the Database or not given i provide you the database schema and the user question you have to respond if the question asked by the user is relevant to the database schema, if it is not related to the databse simply respond with a string NOT_RELATED only the string NOT_RELATED no backticks or any special charachters if it is related to my database respond with RELATED string only you can assume that if in user question database word is there or table word is there or any other word that resembles from any field of SQL it is Valid and you should return RELATED otherwise return NOT_RELATED also if there is word in general in the user query return NOT_RELATED and if the word database is written it should return RELATED
+    <SCHEMA>{get_schemas}</SCHEMA>
+    Question: {user_query}
+    example: how are you
+    response: NOT_RELATED
+    example:What is the average age of females in the USA in general?
+    response:NOT_RELATED
+    example:What is the average age of females in the USA in my database?
+    response:RELATED
+    example: What do you do
+    response: NOT_RELATED
+    example: how many tables are there in my database
+    response: RELATED
+    example: how many person are not male in my database
+    response:RELATED
+    """
+    
+    
+    model = genai.GenerativeModel("gemini-pro")
+    response=model.generate_content([prompt])
+    return response.text
     
 def get_response(user_query:str, db:SQLDatabase,chat_history:list):
     sql_chain=get_sql_chain(db)
     
+
     template="""
-    You are a Data Analyst at a company. You are interactig with a user who is asking you questions about the company's database. Based on the table schema below,question,sql query, and sql response, write a natural language response.
+    You are a Data Analyst at a company.you can generate responses other than database also and You are interacting with a user who is asking you questions about the company's database.using all the tables in the schema and using all the information required from the database , Based on the table schema below,question,sql query, and sql response, write a natural language response as an output. use the whole database schema and give accurate results the output should contain only the response not like Natural Lanaguage Response: and then response only the response
     <SCHEMA>{schema}</SCHEMA>
+    example:how many tables are there in my database
+    response: there are 3 tables in the database 
     
     Conversation History:{chat_history}
     SQL Query:<SQL>{query}</SQL>
     User question: {question}
     SQL Response:{response}
     """
-    
+
     #we passs the sql chain in order to get query it is going to return some variables like query
     prompt=ChatPromptTemplate.from_template(template)
     llm = ChatGoogleGenerativeAI(model="gemini-pro")
@@ -84,7 +118,6 @@ def get_response(user_query:str, db:SQLDatabase,chat_history:list):
         |llm
         |StrOutputParser()
     )
-    
     return chain.invoke({
         "question":user_query,
         "chat_history":chat_history,
@@ -97,11 +130,11 @@ with st.sidebar:
     st.subheader("Settings")
     st.write("This is a single chat application using MYSQL. Connect to the Database and start chatting.")
     
-    st.text_input("Host",value="localhost",key="Host")
+    st.text_input("Host",value="sql6.freesqldatabase.com",key="Host")
     st.text_input("Port",value="3306",key="Port")
-    st.text_input("User",value="root",key="User")
-    st.text_input("Password",type="password",value="admin",key="Password")
-    st.text_input("Database",value="chinook",key="Database")
+    st.text_input("User",value="sql6705444",key="User")
+    st.text_input("Password",type="password",value="NqySep38km",key="Password")
+    st.text_input("Database",value="sql6705444",key="Database")
     
     if st.button("Connect"):
         with st.spinner("Connecting to Database...."):
@@ -136,7 +169,13 @@ if user_query is not None and user_query.strip!="":
         st.markdown(user_query)
     
     with st.chat_message("AI"):
-        response=get_response(user_query,st.session_state.db,st.session_state.chat_history)
-        st.markdown(response)
-    
-    st.session_state.chat_history.append(AIMessage(content=response))
+        
+        if is_valid(user_query,SQLDatabase)=="NOT_RELATED":
+            model = genai.GenerativeModel("gemini-pro")
+            response=model.generate_content([user_query])
+            st.markdown(response.text)
+            st.session_state.chat_history.append(AIMessage(content=response.text))
+        else:   
+            response=get_response(user_query,st.session_state.db,st.session_state.chat_history)
+            st.markdown(response)
+            st.session_state.chat_history.append(AIMessage(content=response))
